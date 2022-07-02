82578aaeac9e7f5a309a62f2a304befe087e271d
陈泽宣 <qchenzexuan@vip.qq.com>
2022-07-02 12:01:32 +0800

    Initial commit

---
+++ src/test/java/org/chenzx/island/IslandApplicationTests.java
@@ -0,0 +1,13 @@
+package org.chenzx.island;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IslandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
